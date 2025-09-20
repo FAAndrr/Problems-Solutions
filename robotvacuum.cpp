8377bdecd351fcfb951b9cf86245b751efeb5489
FAAndrr <164868090+FAAndrr@users.noreply.github.com>
2025-09-20 22:27:11 +1000

    Update robotvacuum.cpp

--- robotvacuum.cpp
+++ robotvacuum.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 int n;
 int x;
@@ -10,7 +8,6 @@ string ins;
 int main() {
     cin >> n;
     cin >> ins;
-    vector<char> letters(ins.begin(),ins.end());
     for(int i = 0; i < n; i++ ){
         if (ins[i] == 'N'){
             y += 1;
