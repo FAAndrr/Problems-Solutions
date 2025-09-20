6fc0dd7c38422a4a12bf9789f4f96017cf41fd5c
FAAndrr <164868090+FAAndrr@users.noreply.github.com>
2025-09-20 22:25:55 +1000

    Add files via upload

---
+++ robotvacuum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int x;
+int y;
+string ins;
+
+int main() {
+    cin >> n;
+    cin >> ins;
+    vector<char> letters(ins.begin(),ins.end());
+    for(int i = 0; i <= n; i++ ){
+        if (ins[i] == 'N'){
+            y += 1;
+        }
+        if (ins[i] == 'E'){
+            x += 1;
+        }
+        if (ins[i] == 'S'){
+            y -= 1;
+        }
+        if (ins[i] == 'W'){
+            x -= 1;
+        }   
+    }    
+    int num = abs(x) + abs(y);
+    cout << num << endl;
+    return 0;
+}
